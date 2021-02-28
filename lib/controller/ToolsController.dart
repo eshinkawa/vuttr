1d9066ac1881d7a4c0044c286acb3731a136dd46
eshinkawa <eshinkawa@gmail.com>
2021-02-28 19:38:51 -0300

    feat: adding layout and delete/get tools

--- lib/controller/ToolsController.dart
+++ lib/controller/ToolsController.dart
@@ -6,14 +6,13 @@ import 'package:VUTTR/shared/consts_api.dart';
 import 'package:http/http.dart' as http;
 
 class ToolsController {
-  String BASE_URL = Platform.isIOS
+  String baseUrl = Platform.isIOS
       ? ApiConstants.BASE_URL_IOS
       : ApiConstants.BASE_URL_ANDROID;
   Future<List<Tool>> getAllTools() async {
     try {
-      print('hello');
-      List<Tool> listTools = List();
-      final response = await http.get(BASE_URL);
+      List<Tool> listTools = [];
+      final response = await http.get(baseUrl);
       if (response.statusCode == 200) {
         var decodeJson = jsonDecode(response.body);
         decodeJson.forEach((item) => listTools.add(Tool.fromJson(item)));
@@ -25,6 +24,57 @@ class ToolsController {
     } catch (error) {
       print("Error loading tools" + error.toString());
       return null;
+    }
+  }
+
+  Future<List<Tool>> searchToolByTerm(String term) async {
+    List<Tool> listTools = [];
+    try {
+      final response = await http.get('$baseUrl?q=$term');
+      if (response.statusCode == 200) {
+        var decodeJson = jsonDecode(response.body);
+        decodeJson.forEach((item) => listTools.add(Tool.fromJson(item)));
+        return listTools;
+      } else {
+        print("Error loading tools" + response.statusCode.toString());
+        return null;
+      }
+    } catch (error) {
+      print("Error loading tools" + error.toString());
+      return null;
+    }
+  }
+
+  Future<List<Tool>> searchToolByQuery(String query) async {
+    List<Tool> listTools = [];
+    try {
+      final response = await http.get('$baseUrl?tags_like=$query');
+      if (response.statusCode == 200) {
+        var decodeJson = jsonDecode(response.body);
+        decodeJson.forEach((item) => listTools.add(Tool.fromJson(item)));
+        return listTools;
+      } else {
+        print("Error loading tools" + response.statusCode.toString());
+        return null;
+      }
+    } catch (error) {
+      print("Error loading tools" + error.toString());
+      return null;
+    }
+  }
+
+  Future<String> deleteTool(int id) async {
+    try {
+      final response = await http.delete('$baseUrl/$id');
+      if (response.statusCode == 200) {
+        return 'Item with id ${id} was deleted';
+      } else {
+        print("Error loading tools" + response.statusCode.toString());
+        return 'Item with id ${id} was not deleted';
+      }
+    } catch (error) {
+      print("Error loading tools" + error.toString());
+      return 'Item with id ${id} was not deleted';
     }
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:VUTTR/controller/ToolsController.dart';
 import 'package:VUTTR/models/tool_model.dart';
+import 'package:VUTTR/styles/index.dart';
+import 'package:VUTTR/widgets/card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -10,10 +13,77 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   var _toolsController = ToolsController();
   List<Tool> tools;
+  bool loading = false;
+  bool isOnlyTags = false;
 
   fetchData() async {
+    loading = true;
     tools = await _toolsController.getAllTools();
+    loading = false;
     setState(() {});
+  }
+
+  queryTool(String query) async {
+    loading = true;
+    if (isOnlyTags) {
+      tools = await _toolsController.searchToolByQuery(query);
+    } else {
+      tools = await _toolsController.searchToolByTerm(query);
+    }
+    loading = false;
+    setState(() {});
+  }
+
+  deleteTool(int id, BuildContext context) async {
+    print(id);
+    loading = true;
+    await _toolsController.deleteTool(id);
+    tools = await _toolsController.getAllTools();
+    loading = false;
+    setState(() {});
+    Navigator.of(context).pop();
+  }
+
+  deleteConfirmation(String title, int id) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("REMOVE TOOL",
+              style: TextStyle(
+                fontFamily: Font.FONT_FAMILY,
+                fontWeight: FontWeight.w700,
+              )),
+          content: Text("Are you sure you want to remove ${title}",
+              style: TextStyle(
+                fontFamily: Font.FONT_FAMILY,
+                fontWeight: FontWeight.w400,
+              )),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text("Cancel",
+                  style: TextStyle(
+                    fontFamily: Font.FONT_FAMILY,
+                    fontWeight: FontWeight.w600,
+                  )),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: Text("Yes, remove",
+                  style: TextStyle(
+                    fontFamily: Font.FONT_FAMILY,
+                    fontWeight: FontWeight.w600,
+                  )),
+              onPressed: () {
+                deleteTool(id, context);
+              },
+            )
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -26,13 +96,97 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ListView.builder(
-          itemCount: tools.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${tools[index].description}'),
-            );
-          },
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'VUTTR',
+                      style: TextStyle(
+                        fontSize: 48.0,
+                        fontFamily: Font.FONT_FAMILY,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 0.8),
+                      borderRadius: BorderRadius.circular(32.0)),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 7,
+                        child: TextFormField(
+                          onChanged: (query) => queryTool(query),
+                          keyboardType: TextInputType.text,
+                          autofocus: false,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontFamily: Font.FONT_FAMILY,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search),
+                            hintText: 'search',
+                            contentPadding: EdgeInsets.all(0.0),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(32.0)),
+                          ),
+                          // validator: FormValidator().validateEmail,
+                          onSaved: (String value) {},
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Row(
+                          children: [
+                            Text('only tags',
+                                style: TextStyle(
+                                  fontFamily: Font.FONT_FAMILY,
+                                  fontWeight: FontWeight.w600,
+                                )),
+                            Checkbox(
+                              value: isOnlyTags,
+                              onChanged: (bool value) {
+                                isOnlyTags = value;
+                                setState(() {});
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: loading
+                      ? Center(child: CupertinoActivityIndicator())
+                      : ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: tools.length,
+                          itemBuilder: (context, index) => ToolCard(
+                              title: tools[index].title,
+                              description: tools[index].description,
+                              tags: tools[index].tags,
+                              link: tools[index].link,
+                              onDelete: () => deleteConfirmation(
+                                    tools[index].title,
+                                    tools[index].id,
+                                  )),
+                        ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
